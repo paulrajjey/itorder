99253df837deb56c60df1b83f9e4b44681a3845a
rhpamAdmin <>
2020-06-07 16:33:26 -0500

    {/src/main/java/org/jbpm/demo/itorders/Order.java}

--- src/main/java/org/jbpm/demo/itorders/Order.java
+++ src/main/java/org/jbpm/demo/itorders/Order.java
@@ -6,12 +6,43 @@ package org.jbpm.demo.itorders;
 
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Order() {
-    }
+	private java.lang.String id;
+	private java.lang.String name;
+	private double amount;
 
+	public Order() {
+	}
 
+	public java.lang.String getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.String id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public double getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Order(java.lang.String id, java.lang.String name, double amount) {
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+	}
 
 }
